bae9afe6859b95c7059bdadb59fc58418676c07c
algonacci <algonacci1@gmail.com>
2022-03-24 21:54:45 +0700

    updates profile

---
+++ src/components/Welcome.svelte
@@ -0,0 +1,45 @@
+<section class="header">
+  <div class="center">
+      <img class="mt-5" src="assets/images/profile.jpg" alt="Profil">
+      <h1 class="mt-2">Hello! I'm Eric👋</h1>
+  </div>
+</section>
+
+<section class="introduction container p-5">
+    <div>
+        <p>
+            Hello, I'm Eric Julianto. Currently studying Hospitality and Tourism (third-year) at Universitas Bunda Mulia Ancol.
+            I extremely motivated to constantly develop my skills and grow.
+            Currently I'm studying about technologies such as web development, cross-platform development, artificial intelligence, internet of things, and many more. You can find me in these platform:
+        </p>
+    </div>
+
+    <div class="contact">
+        <a target="_blank" href="https://github.com/algonacci"><i class="fab fa-github text-white px-3"></i></a>
+        <a target="_blank" href="https://www.instagram.com/ericjuliantooo/"><i class="fab fa-instagram text-white px-3"></i></a>
+        <a target="_blank" href="https://www.linkedin.com/in/ericjulianto/"><i class="fab fa-linkedin text-white px-3"></i></a>
+    </div>
+</section>
+
+
+
+<style>
+    .center {
+        margin: auto;
+        text-align: center;
+    }
+
+    img {
+        width: 300px;
+        border-radius: 300px;
+    }
+
+    .introduction {
+        text-align: justify;;
+    }
+
+    .contact {
+        text-align: center;
+        font-size: 3em;
+    }
+</style>
