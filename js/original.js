c25eb1ca2aa62bb8ec97654d0f858a98c029c683
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-10-07 15:16:20 +0700

    update

--- js/original.js
+++ js/original.js
@@ -44,10 +44,7 @@
       if (!navbarlink.hash) return;
       let section = select(navbarlink.hash);
       if (!section) return;
-      if (
-        position >= section.offsetTop &&
-        position <= section.offsetTop + section.offsetHeight
-      ) {
+      if (position >= section.offsetTop && position <= section.offsetTop + section.offsetHeight) {
         navbarlink.classList.add("active");
       } else {
         navbarlink.classList.remove("active");
